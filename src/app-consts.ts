da16c88394b5fdc10a63553764ddcd4e4bd3a2e9
Kamil Abzalov <mail@kamil-abzalov.com>
2023-07-30 12:16:15 +0300

    Refactoring

---
+++ src/app-consts.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'https://app.admss.com/api/v1/'
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,35 +1,34 @@
 import axios from "axios";
-import { getToken } from "./utils";
+import {getToken} from "./utils";
+import {API_URL} from "../app-consts";
 
 export interface LoginResponse {
-  modified: string;
-  sessionuid: string;
-  started: string;
-  status: "OK";
-  token: string;
-  useruid: string;
+    modified: string;
+    sessionuid: string;
+    started: string;
+    status: "OK";
+    token: string;
+    useruid: string;
 }
 
 export interface LogoutResponse {
-  status: "OK";
+    status: "OK";
 }
 
-const API_URL = "http://app.admss.com:8088/api/v1/";
-
 export const login = (username: string, password: string) => {
-  return axios
-    .post<LoginResponse>(`${API_URL}user`, {
-      user: username,
-      secret: password,
-      magic: "avansoft",
-    })
-    .then((response) => response.data);
+    return axios
+        .post<LoginResponse>(`${API_URL}user`, {
+            user: username,
+            secret: password,
+            magic: "avansoft",
+        })
+        .then((response) => response.data);
 };
 
 export const logout = (userId: string) => {
-  return axios
-    .post<LogoutResponse>(`${API_URL}user/${userId}/logout`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    })
-    .then((response) => response.data);
+    return axios
+        .post<LogoutResponse>(`${API_URL}user/${userId}/logout`, null, {
+            headers: {Authorization: `Bearer ${getToken()}`},
+        })
+        .then((response) => response.data);
 };
