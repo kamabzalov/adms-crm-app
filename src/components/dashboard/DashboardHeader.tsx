bbeb359b17058619829c09905deba516d4918fb0
JSDID <icegouse@gmail.com>
2023-09-04 14:33:07 +0300

    Add user actions link

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -22,21 +22,25 @@ export function DashboardHeader() {
                         <div className='menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px 2 px-lg-0'>
                             <div className='menu-item me-lg-1'>
                                 <Link className='menu-link py-3' to={'/dashboard'}>
+                                    <i className='ki-outline ki-user-tick fs-2 m-2'></i>
                                     <span className='menu-title'>Users</span>
                                 </Link>
                             </div>
                             <div className='menu-item me-lg-1'>
                                 <Link className='menu-link py-3' to={'/dashboard/billing'}>
+                                    <i className='ki-outline ki-bill fs-2 m-2'></i>
                                     <span className='menu-title'>Billing</span>
                                 </Link>
                             </div>
                             <div className='menu-item me-lg-1'>
                                 <Link className='menu-link py-3' to={'/dashboard/reports'}>
+                                    <i className='ki-outline ki-notepad fs-2 m-2'></i>
                                     <span className='menu-title'>Reports</span>
                                 </Link>
                             </div>
                             <div className='menu-item me-lg-1'>
                                 <span onClick={() => signOut()} className='menu-link'>
+                                    <i className='ki-outline ki-exit-right fs-2 m-2'></i>
                                     <span className='menu-title'>Log out</span>
                                 </span>
                             </div>
