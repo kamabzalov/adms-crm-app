6f4aee566e999a90248c158c0dbd0f2fcca067a9
JSDID <icegouse@gmail.com>
2023-09-12 17:08:45 +0300

    Fix responsive header

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { useFormik } from 'formik';
-import { HTMLInputTypeAttribute, useState } from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
 
 import { login } from '../services/auth.service';
@@ -10,8 +10,6 @@ interface LoginCredentials {
     username: string;
     password: string;
 }
-
-type PasswordFieldIcon = 'ki-eye' | 'ki-eye-slash';
 
 const loginSchema = Yup.object().shape({
     username: Yup.string().trim().required('Username is required'),
@@ -25,26 +23,7 @@ const initialValues: LoginCredentials = {
 
 export function Login() {
     const [loading, setLoading] = useState(false);
-    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
-    const [passwordFieldType, setPasswordFieldType] = useState<HTMLInputTypeAttribute>('password');
-    const [passwordFieldIcon, setPasswordFieldIcon] = useState<PasswordFieldIcon>('ki-eye');
-
     const navigate = useNavigate();
-
-    const handleChangePasswordField = () => {
-        switch (isPasswordVisible) {
-            case true:
-                setPasswordFieldType('password');
-                setPasswordFieldIcon('ki-eye-slash');
-                setIsPasswordVisible(false);
-                break;
-            case false:
-                setPasswordFieldType('text');
-                setPasswordFieldIcon('ki-eye');
-                setIsPasswordVisible(true);
-                break;
-        }
-    };
 
     const formik = useFormik({
         initialValues,
@@ -109,12 +88,12 @@ export function Login() {
                             )}
                         </div>
 
-                        <div className='fv-row mb-8 position-relative'>
+                        <div className='fv-row mb-8'>
                             <label className='form-label fw-bolder text-dark fs-6 mb-0'>
                                 Password
                             </label>
                             <input
-                                type={passwordFieldType}
+                                type='password'
                                 placeholder='Password'
                                 autoComplete='off'
                                 {...formik.getFieldProps('password')}
@@ -137,12 +116,6 @@ export function Login() {
                                     </div>
                                 </div>
                             )}
-                            <i
-                                className={clsx(
-                                    `ki-outline fs-2 ${passwordFieldIcon} position-absolute end-0 top-50 px-3 cursor-pointer text-hover-primary`
-                                )}
-                                onClick={handleChangePasswordField}
-                            />
                         </div>
 
                         <div className='d-grid'>
