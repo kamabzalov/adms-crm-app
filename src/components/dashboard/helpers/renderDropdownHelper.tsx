4b65ae155aba7bc5b9f549782138a95f996913f1
JSDID <icegouse@gmail.com>
2023-09-14 09:40:04 +0300

    Add profile dropdowm menu with name and logo ut

--- src/components/dashboard/helpers/renderDropdownHelper.tsx
+++ src/components/dashboard/helpers/renderDropdownHelper.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { FC, useEffect } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { MenuComponent } from '_metronic/assets/ts/components';
 
 interface PropsItems {
     menuItemName: string;
-    menuItemAction: () => void;
+    menuItemAction?: () => void;
 }
 
-interface Props {
+interface DropdownProps {
     title: string;
-    items: PropsItems[];
+    items?: PropsItems[];
 }
 
-export const CustomDropdown: FC<Props> = ({ title, items }) => {
+export const CustomDropdown = ({ title, items, children }: PropsWithChildren<DropdownProps>) => {
     useEffect(() => {
         MenuComponent.reinitialization();
     }, []);
@@ -20,7 +20,6 @@ export const CustomDropdown: FC<Props> = ({ title, items }) => {
     return (
         <>
             <a
-                href='/#'
                 className='btn btn-light btn-active-light-primary btn-sm'
                 data-kt-menu-trigger='click'
                 data-kt-menu-placement='bottom-end'
@@ -32,11 +31,13 @@ export const CustomDropdown: FC<Props> = ({ title, items }) => {
                 className='menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-150px py-4'
                 data-kt-menu='true'
             >
-                {items.map(({ menuItemName, menuItemAction }) => (
-                    <div key={menuItemName} className='menu-item px-3' onClick={menuItemAction}>
-                        <a className='menu-link px-3'>{menuItemName}</a>
-                    </div>
-                ))}
+                {children}
+                {items &&
+                    items.map(({ menuItemName, menuItemAction }) => (
+                        <div key={menuItemName} className='menu-item px-3' onClick={menuItemAction}>
+                            <a className='menu-link px-3'>{menuItemName}</a>
+                        </div>
+                    ))}
             </div>
         </>
     );
---
+++ src/components/services/auth.service.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { getToken } from './utils';
+import { API_URL } from '../app-consts';
+
+export interface LoginResponse {
+    companyname: string;
+    firstname: string;
+    isadmin: number;
+    islocaladmin: number;
+    ismanager: number;
+    issalesperson: number;
+    lastname: string;
+    loginname: string;
+    sessionuid: string;
+    status: 'OK';
+    token: string;
+    username: string;
+    useruid: string;
+}
+
+export interface LogoutResponse {
+    status: 'OK';
+}
+
+export const login = (username: string, password: string) => {
+    return axios
+        .post<LoginResponse>(`${API_URL}user`, {
+            user: username,
+            secret: password,
+            magic: 'avansoft',
+        })
+        .then((response) => response.data);
+};
+
+export const logout = (userId: string) => {
+    return axios
+        .post<LogoutResponse>(`${API_URL}user/${userId}/logout`, null, {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        })
+        .then((response) => response.data);
+};
