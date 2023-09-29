c7f0033d635c62efdba7034f4eff0d78fa6ecf75
JSDID <icegouse@gmail.com>
2023-09-29 18:08:25 +0300

    Refactoring UserModal

--- src/components/dashboard/users/UserModal/parts/UserModal.tsx
+++ src/components/dashboard/users/UserModal/parts/UserModal.tsx
@@ -1,11 +1,11 @@
+import { AxiosError } from 'axios';
 import clsx from 'clsx';
 import * as Yup from 'yup';
-import { useFormik } from 'formik';
-import { useState } from 'react';
 import { UserData } from 'common/interfaces/UserData';
+import { useToast, TOAST_DURATION } from 'components/dashboard/helpers/renderToastHelper';
+import { useFormik } from 'formik';
+import { HTMLInputTypeAttribute, useState } from 'react';
 import { User, createOrUpdateUser } from 'services/user.service';
-import { TOAST_DURATION, useToast } from 'components/dashboard/helpers/renderToastHelper';
-import { AxiosError } from 'axios';
 
 interface UserModalProps {
     onClose: () => void;
@@ -13,26 +13,82 @@ interface UserModalProps {
     updateData?: () => void;
 }
 
+interface UserModalData extends UserData {
+    confirmPassword: '';
+}
+
+// eslint-disable-next-line no-unused-vars
+enum PassIcon {
+    // eslint-disable-next-line no-unused-vars
+    SHOW = 'ki-eye',
+    // eslint-disable-next-line no-unused-vars
+    HIDDEN = 'ki-eye-slash',
+}
+
 export const UserModal = ({ onClose, user, updateData }: UserModalProps): JSX.Element => {
-    const [hasServerError, setHasServerError] = useState<boolean>(false);
-    const initialUserData: UserData = {
+    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+    const [passwordFieldType, setPasswordFieldType] = useState<HTMLInputTypeAttribute>('password');
+    const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState<boolean>(false);
+    const [confirmPasswordFieldType, setConfirmPasswordFieldType] =
+        useState<HTMLInputTypeAttribute>('password');
+    const [, setHasServerError] = useState<boolean>(false);
+
+    const initialUserData: UserModalData = {
         username: user?.username || '',
         password: '',
+        confirmPassword: '',
     };
 
     const { handleShowToast } = useToast();
 
-    const [userData] = useState<UserData>(initialUserData);
+    const handleChangePasswordVisible = () => {
+        switch (isPasswordVisible) {
+            case true:
+                setPasswordFieldType('password');
+                setIsPasswordVisible(false);
+                break;
+            case false:
+                setPasswordFieldType('text');
+                setIsPasswordVisible(true);
+                break;
+        }
+    };
+
+    const handleChangeConfirmPasswordVisible = () => {
+        switch (isConfirmPasswordVisible) {
+            case true:
+                setConfirmPasswordFieldType('password');
+                setIsConfirmPasswordVisible(false);
+                break;
+            case false:
+                setConfirmPasswordFieldType('text');
+                setIsConfirmPasswordVisible(true);
+                break;
+        }
+    };
 
     const addUserSchema = Yup.object().shape({
         username: Yup.string().trim().required('Username is required'),
         password: Yup.string().trim().required('Password is required'),
+        confirmPassword: Yup.string()
+            .trim()
+            .oneOf([Yup.ref('password')], 'Passwords must match')
+            .required('Password confirmation is required'),
     });
 
     const formik = useFormik({
-        initialValues: userData,
+        initialValues: initialUserData,
         validationSchema: addUserSchema,
-        onSubmit: async ({ username, password }, { setSubmitting }) => {
+        onSubmit: async ({ username, password, confirmPassword }, { setSubmitting }) => {
+            if (password !== confirmPassword) {
+                handleShowToast({
+                    message: 'Passwords do not match',
+                    type: 'danger',
+                });
+                setSubmitting(false);
+                return;
+            }
+
             setSubmitting(true);
             try {
                 const params: [string, string, string?] = [username, password];
@@ -97,30 +153,82 @@ export const UserModal = ({ onClose, user, updateData }: UserModalProps): JSX.El
                         )}
                     </div>
 
-                    <div className='fv-row mb-8'>
+                    <div className='fv-row mb-10 position-relative'>
                         <label className='form-label fw-bolder text-dark fs-6 mb-0'>Password</label>
                         <input
-                            type='password'
+                            type={passwordFieldType}
                             placeholder='Password'
                             autoComplete='off'
                             {...formik.getFieldProps('password')}
                             className={clsx(
-                                'form-control',
+                                'form-control bg-transparent',
                                 {
-                                    'is-invalid': formik.touched.password && formik.errors.password,
+                                    'border-danger':
+                                        formik.touched.password && formik.errors.password,
                                 },
                                 {
-                                    'is-valid': formik.touched.password && !formik.errors.password,
+                                    'border-success':
+                                        formik.touched.password && !formik.errors.password,
                                 }
                             )}
                         />
                         {formik.touched.password && formik.errors.password && (
-                            <div className='fv-plugins-message-container'>
+                            <div className='fv-plugins-message-container position-absolute'>
                                 <div className='fv-help-block'>
                                     <span role='alert'>{formik.errors.password}</span>
                                 </div>
                             </div>
                         )}
+
+                        <i
+                            className={clsx(
+                                `ki-outline fs-2 ${
+                                    isPasswordVisible ? PassIcon.SHOW : PassIcon.HIDDEN
+                                } position-absolute end-0 top-50 px-3 cursor-pointer text-hover-primary`
+                            )}
+                            onClick={handleChangePasswordVisible}
+                        />
+                    </div>
+
+                    <div className='fv-row mb-10 position-relative'>
+                        <label className='form-label fw-bolder text-dark fs-6 mb-0'>
+                            Password Confirmation
+                        </label>
+                        <input
+                            type={confirmPasswordFieldType}
+                            placeholder='Confirm Password'
+                            autoComplete='off'
+                            {...formik.getFieldProps('confirmPassword')}
+                            className={clsx(
+                                'form-control bg-transparent',
+                                {
+                                    'border-danger':
+                                        formik.touched.confirmPassword &&
+                                        formik.errors.confirmPassword,
+                                },
+                                {
+                                    'border-success':
+                                        formik.touched.confirmPassword &&
+                                        !formik.errors.confirmPassword,
+                                }
+                            )}
+                        />
+                        {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+                            <div className='fv-plugins-message-container position-absolute'>
+                                <div className='fv-help-block'>
+                                    <span role='alert'>{formik.errors.confirmPassword}</span>
+                                </div>
+                            </div>
+                        )}
+
+                        <i
+                            className={clsx(
+                                `ki-outline fs-2 ${
+                                    isConfirmPasswordVisible ? PassIcon.SHOW : PassIcon.HIDDEN
+                                } position-absolute end-0 top-50 px-3 cursor-pointer text-hover-primary`
+                            )}
+                            onClick={handleChangeConfirmPasswordVisible}
+                        />
                     </div>
                 </div>
                 <div className='text-center pt-15'>
