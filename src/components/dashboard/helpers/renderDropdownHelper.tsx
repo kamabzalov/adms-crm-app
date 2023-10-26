cee6a6ae910ae0f65e3710d67c4de022af4f5117
JSDID <icegouse@gmail.com>
2023-10-26 11:59:36 +0300

    Added password change to the top right dropdown

--- src/components/dashboard/helpers/renderDropdownHelper.tsx
+++ src/components/dashboard/helpers/renderDropdownHelper.tsx
@@ -4,15 +4,22 @@ import { PropsWithChildren, useEffect } from 'react';
 
 interface PropsItems {
     menuItemName: string;
+    icon?: string;
     menuItemAction?: () => void;
 }
 
 interface DropdownProps {
     title: string;
+    weight?: number;
     items?: PropsItems[];
 }
 
-export const CustomDropdown = ({ title, items, children }: PropsWithChildren<DropdownProps>) => {
+export const CustomDropdown = ({
+    title,
+    items,
+    children,
+    weight,
+}: PropsWithChildren<DropdownProps>) => {
     useEffect(() => {
         MenuComponent.reinitialization();
     }, []);
@@ -28,14 +35,19 @@ export const CustomDropdown = ({ title, items, children }: PropsWithChildren<Dro
                 <i className='ki-duotone ki-down fs-5 m-0'></i>
             </a>
             <div
-                className='menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-150px py-4'
+                className={`menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-${
+                    weight || 150
+                }px py-4`}
                 data-kt-menu='true'
             >
                 {children}
                 {items &&
-                    items.map(({ menuItemName, menuItemAction }) => (
+                    items.map(({ menuItemName, menuItemAction, icon }) => (
                         <div key={menuItemName} className='menu-item px-3' onClick={menuItemAction}>
-                            <a className='menu-link px-3'>{menuItemName}</a>
+                            <a className='menu-link px-3 text-hover-primary'>
+                                {icon && <i className={`ki-outline ki-${icon} fs-2 me-2`} />}
+                                {menuItemName}
+                            </a>
                         </div>
                     ))}
             </div>
