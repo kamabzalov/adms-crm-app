8d5324501fb814431a4079714b17c3c5c1311c18
JSDID <icegouse@gmail.com>
2023-11-19 11:28:09 +0200

    Remove Add users

--- src/components/dashboard/users/Users.tsx
+++ src/components/dashboard/users/Users.tsx
@@ -1,16 +1,30 @@
 import { QueryRequestProvider } from 'common/core/QueryRequestProvider';
 import { QueryResponseProvider } from 'common/core/QueryResponseProvider';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CustomModal } from '../helpers/modal/renderModalHelper';
 import { PrimaryButton } from '../smallComponents/buttons/PrimaryButton';
 import { UsersListSearchComponent } from '../smallComponents/search/Search';
 import { UserModal } from './UserModal/parts/UserModal';
 import { UsersTable } from './table/UsersTable';
+import { UserContext } from 'Content';
+import { UserPermissions } from 'common/interfaces/UserData';
 
 export const Users = () => {
     const [addUserModalEnabled, setAddUserModalEnabled] = useState<boolean>(false);
-
     const handleAddUserModalOpen = () => setAddUserModalEnabled(!addUserModalEnabled);
+
+    const { userPermission } = useContext(UserContext);
+
+    const [permission, setPermission] = useState('');
+
+    useEffect(() => {
+        setPermission(userPermission);
+    }, [userPermission]);
+
+    const isUserPermission =
+        permission === UserPermissions.ADMIN ||
+        permission === UserPermissions.LOCAL_ADMIN ||
+        permission === UserPermissions.MANAGER;
 
     return (
         <QueryRequestProvider>
@@ -25,12 +39,14 @@ export const Users = () => {
                         <div className='tab-content' id='myTabContentInner'>
                             <div className='d-flex w-100 justify-content-between my-4'>
                                 <UsersListSearchComponent />
-                                <PrimaryButton
-                                    icon='plus'
-                                    buttonClickAction={handleAddUserModalOpen}
-                                >
-                                    Add User
-                                </PrimaryButton>
+                                {isUserPermission && (
+                                    <PrimaryButton
+                                        icon='plus'
+                                        buttonClickAction={handleAddUserModalOpen}
+                                    >
+                                        Add User
+                                    </PrimaryButton>
+                                )}
                             </div>
                             <UsersTable />
                         </div>
