56b012a8445bb94448446f7da546846f59be68ed
Alexander <102165823+JSDID@users.noreply.github.com>
2024-02-09 20:27:24 +0200

    Refactoring Form (#240)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'styles.scss';
 
 import './_metronic/assets/fonticon/fonticon.css';
 import './_metronic/assets/keenicons/duotone/style.css';
